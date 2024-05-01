0ceb3994fd70f0eb556fe18c5ff44d13fb34056a
suminkim <di05023@gmail.com>
2024-05-01 15:29:20 +0900

    수강신청 (1920px) 작업 완료
    자원봉사 페이지 제작 해야함

--- js/edu_list.js
+++ js/edu_list.js
@@ -1,12 +1,48 @@
 window.addEventListener("load", function () {
-  // 헤더 스크롤 이벤트
   window.addEventListener("scroll", function () {
     var headerWrap = document.querySelector(".header-wrap");
     if (window.scrollY > 0) {
-      headerWrap.classList.add("scrolled");
+      headerWrap.classList.add("scrolled"); // 스크롤된 경우 scrolled 클래스 추가
     } else {
-      headerWrap.classList.remove("scrolled");
+      headerWrap.classList.remove("scrolled"); // 스크롤이 상단에 위치한 경우 scrolled 클래스 제거
     }
+  });
+
+  // 반응형 (1024px) 햄버거바가 생겼을때
+  // 햄버거버튼을 클릭했을때
+  const navMb = document.querySelector(".nav-mb");
+  const htmlRoot = document.querySelector("html");
+  const mbt = document.querySelector(".mbt");
+
+  mbt.addEventListener("click", function () {
+    const state = this.classList.contains("ani");
+    if (state) {
+      // 햄버거 버튼을  눌렀을때 x가 바뀌는 코드
+      this.classList.remove("ani");
+      // 모바일에 메뉴가 나타나는 코드
+      navMb.classList.remove("active");
+      // 스크롤이 안생기게 하는 코드
+      htmlRoot.classList.remove("active");
+    } else {
+      // 햄버거 버튼을  눌렀을때 x가 바뀌는 코드
+      this.classList.add("ani");
+      // 모바일에 메뉴가 나타나는 코드
+      navMb.classList.add("active");
+      // 스크롤이 안생기게 하는 코드
+      htmlRoot.classList.add("active");
+    }
+  });
+  var menu1 = document.querySelector(".hd-menu1 > a");
+  var menu2 = document.querySelector(".hd-menu2");
+  menu1.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu2.classList.toggle("active");
+  });
+  var menu3 = document.querySelector("#volunteer-apply > a");
+  var menu4 = document.querySelector(".hd-menu3");
+  menu3.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu4.classList.toggle("active");
   });
 
   // 페이지 내비게이션 렌더링
@@ -207,7 +243,6 @@ window.addEventListener("load", function () {
         return item.dataValue === newData.dataValue && item.areaValue === newData.areaValue && item.centerValue === newData.centerValue;
       });
 
-
       // 중복된 값이 아닌 경우에만 데이터를 저장합니다.
       if (!isDuplicate) {
         storedData.push(newData);
@@ -215,7 +250,6 @@ window.addEventListener("load", function () {
       }
     }
   });
-
 
   // 페이지 로드 시 초기 강의 목록 렌더링
   window.addEventListener("load", function () {
