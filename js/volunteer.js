964e4d609003c412fac127b83be7329beb046464
suminkim <di05023@gmail.com>
2024-05-02 16:54:05 +0900

    교육봉사 랜딩페이지 제작 완료 (반응형 x)
    교육봉사 셀렉박스 기본ui 맘에 안들어서 토글 메뉴 제작 중

--- js/volunteer.js
+++ js/volunteer.js
@@ -63,7 +63,93 @@ window.addEventListener("load", function () {
   }
 
   renderPgNavi();
+});
 
-  //  json data
-  
+// 스와이퍼 제이쿼리
+$(document).ready(function () {
+  // json data
+  $.getJSON("data.json")
+    .done(function (data) {
+      if (data && data.lecture_list && data.lecture_list.length > 0) {
+        LECTURE_ARRAY = data.lecture_list;
+        showLecture();
+      } else {
+        console.error("No review data found.");
+      }
+    })
+    .fail(function (jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      console.error("Fetch error:", err);
+    });
+
+  let LECTURE_ARRAY;
+  let lectureSlide = $("#data-swEnrol");
+  function showLecture() {
+    let html = "";
+    $.each(LECTURE_ARRAY, function (idx, item) {
+      var tag = `
+      <div class="swiper-slide">
+      <div class = "enroll-list-li">
+          <div class="enroll-info-left">
+            <img src="${item.imgSrc}" alt="${item.kiosk}" />
+          </div>
+          <div class="enroll-info-right">
+            <h2 class="erinfo-title">${item.lecture_title}</h2>
+            <div class="study-info">
+              <p class="erinfo-subtitle">교육내용</p>
+              <p class="erinfo">${item.erinfo}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+      html += tag;
+    });
+    lectureSlide.html(html);
+  }
+
+  let swLecture = new Swiper(".sw-enrol", {
+    slidesPerView: 1.2,
+    spaceBetween: 35,
+    centeredSlides: true,
+    allowTouchMove: false,
+  });
+
+  // 슬라이드 변경 이벤트 리스너
+  swLecture.on("slideChange", function () {
+    updateButtonStatus();
+  });
+
+  // 초기 버튼 상태 설정
+  updateButtonStatus();
+
+  // 버튼 상태 업데이트 함수
+  function updateButtonStatus() {
+    // 현재 슬라이드의 인덱스 가져오기
+    let currentIndex = swLecture.realIndex;
+
+    // 이전 버튼 상태 업데이트
+    if (currentIndex === 0) {
+      $(".sw-enrol-prev").addClass("disabled"); // 이전 버튼 비활성화
+    } else {
+      $(".sw-enrol-prev").removeClass("disabled"); // 이전 버튼 활성화
+    }
+
+    // 다음 버튼 상태 업데이트
+    if (currentIndex === swLecture.slides.length - 1) {
+      $(".sw-enrol-next").addClass("disabled"); // 다음 버튼 비활성화
+    } else {
+      $(".sw-enrol-next").removeClass("disabled"); // 다음 버튼 활성화
+    }
+  }
+
+  // 이전 버튼 클릭 이벤트 핸들러
+  $(".sw-enrol-prev").on("click", function () {
+    swLecture.slidePrev(); // 이전 슬라이드로 이동
+  });
+
+  // 다음 버튼 클릭 이벤트 핸들러
+  $(".sw-enrol-next").on("click", function () {
+    swLecture.slideNext(); // 다음 슬라이드로 이동
+  });
 });
